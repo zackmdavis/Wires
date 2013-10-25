1d11adff5c12ac3c2494b717dbeb0eefdd831176
Zack M. Davis <code@zackmdavis.net>
2013-10-24 22:43:03 -0700

    corrects location of post views

--- blog_engine/controllers/posts_controller.py
+++ blog_engine/controllers/posts_controller.py
@@ -9,18 +9,18 @@ class PostsController:
     # dictionaries of the request handler.
 
     def index(parameters):
-        template = open('./views/index.html').read()
+        template = open('./views/posts/index.html').read()
         return TemplateEngine(template, parameters).render()
 
     def show(parameters):
         post = Post.find(Post.cxn, "posts", parameters["id"])
-        template = open('./views/show.html').read()
+        template = open('./views/posts/show.html').read()
         attributes = {key:str(post.attributes[key]) for key in post.attributes}
         definitions = dict(list(parameters.items()) + list(attributes.items()))
         return TemplateEngine(template, definitions).render()
 
     def new(parameters):
-        template = open('./views/new.html').read()
+        template = open('./views/posts/new.html').read()
         definitions = {}
         return TemplateEngine(template, definitions).render()
 
