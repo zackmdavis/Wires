4a5eb08a7a49525b990354c02fe0e96d7930c5c3
Zack M. Davis <code@zackmdavis.net>
2013-11-18 16:25:11 -0800

    test_can_sign_in_and_post

--- test/test_blog_engine.py
+++ test/test_blog_engine.py
@@ -5,6 +5,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import os, binascii
+
 class TestLinks(unittest.TestCase):
 
     @classmethod
@@ -73,25 +75,52 @@ class TestAuthentication(unittest.TestCase):
     def setUp(self):
         self.driver.get("http://localhost:8080/")
 
-    def test_sign_in_and_out(self):
-        # shouldn't be logged in to start with
-        auth_msg = self.driver.find_element_by_id("authentication_status").text
-        self.assertIn("not logged in", auth_msg)
-
-        # should be able to log in
+    def log_in(self):
         self.driver.find_element_by_id("login_link").click()
         username_field = self.driver.find_element_by_id("username")
         username_field.send_keys("admin")
         password_field = self.driver.find_element_by_id("password")
         password_field.send_keys("admin")
         self.driver.find_element_by_id("login_submit").click()
+
+    def log_out(self):
+        self.driver.find_element_by_id("logout_link").click()
+
+    def test_sign_in_and_out(self):
+        # shouldn't be logged in to start with
+        auth_msg = self.driver.find_element_by_id("authentication_status").text
+        self.assertIn("not logged in", auth_msg)
+
+        # should be able to log in
+        self.log_in()
         auth_msg = self.driver.find_element_by_id("authentication_status").text
         self.assertIn("logged in as Administrator (admin)", auth_msg)
         
         # should be able to log out
-        self.driver.find_element_by_id("logout_link").click()
+        self.log_out()
         auth_msg = self.driver.find_element_by_id("authentication_status").text
         self.assertIn("not logged in", auth_msg)
+
+    def test_can_sign_in_and_post(self):
+        self.log_in()
+        self.driver.find_element_by_id("new_post_link").click()
+        stamp = str(binascii.b2a_hex(os.urandom(4)), encoding="utf-8")
+        title = "Test Post " + stamp
+        body = "This is the body of {0}.".format(title)
+        title_field = self.driver.find_element_by_id("title")
+        title_field.send_keys(title)
+        body_field = self.driver.find_element_by_id("body")
+        body_field.send_keys(body)
+        author_id_field = self.driver.find_element_by_id("author_id")
+        author_id_field.send_keys("1")
+        self.driver.find_element_by_id("new_post_submit").click()
+        page = self.driver.page_source
+        self.assertIn(stamp, page)
+        self.log_out()
+
+    # not yet implemented
+    def test_cannot_post_if_not_logged_in(self):
+        pass
 
     @classmethod
     def tearDownClass(cls):
